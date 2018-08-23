c615e495ed4847098cad1b2fd22b34e21fb219f5
Justin O'Halloran <justinohallo@gmail.com>
2018-08-23 11:48:08 -0700

    Added Firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,11 +9,13 @@ export class HomePage {
 menuData = [ 
   {title: 'Our menu', pic: 'assets/img/soup1.jpg', pushPage: 'MenuPage'},
   {title: 'Account', pic: 'assets/img/coffee-people3.jpg', pushPage: 'AccountPage'},
-  {title: 'About Us', pic: 'assets/img/coffee6', pushPage: 'AboutPage'},
+  {title: 'About Us', pic: 'assets/img/coffee6.jpg', pushPage: 'AboutPage'},
   {title: 'Locations', pic: 'assets/img/cafe2.jpg', pushPage: 'LocationsPage'}
-]
+];
+
 
 logPage: any
+
   constructor(public navCtrl: NavController) {
     this.logPage= 'LoginPage'
   }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import {HomePage} from "../home/home";
+import { AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase/app'
 /**
  * Generated class for the RegisterPage page.
  *
@@ -15,11 +17,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  reg = {
+    email:'',
+    passWrd1:'',
+    passWrd2:''
+  };
+
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public alertCtrl: AlertController, private afAuth: AngularFireAuth) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  displayAlert(alertTile, alertSub){
+    let theAlert = this.alertCtrl.create({
+      title: alertTitle,
+      subTitle: alertSub,
+      buttons:['OK']
+    })
+    theAlert.present();
+  }
+  registerAccount(){
+    if(this.reg.passWrd1 != this.reg.passWrd2){
+      this.displayAlert('Password Probelem!', 'Passwords do not match, please try again.')
+      this.reg.passWrd1='',
+      this.reg.passWrd2=''
+    }
+    else {
+      this.afAuth.auth.createUserWithEmailAndPassword(this.reg.email, this.reg.passWrd1)
+      .then(res => this.regSuccess(res))
+      .catch(err => this.displayAlert('Error!', err))
+    }
+  }
+
+  regSuccess(result){
+    this.displayAlert(result.email, 'Account created for this email address');
+    this.afAuth.auth.signInWithEmailAndPassword(this.reg.email, this.reg.passWrd1)
+    .then(res => this.navCtrl.push(HomePage))
+    .catch( err => this.displayAlert('Error!', err))
   }
 
 }
